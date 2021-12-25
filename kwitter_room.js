a2e4bf592c57317293d4ed2690a631ecfa8fdc42
Ashwath29 <78354532+Ashwath29@users.noreply.github.com>
2021-12-25 20:33:30 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDIqchHUz7RRDnaO0a_q_zh2zuiUeI9bSI",
+    authDomain: "kwitter-hw-3dc3e.firebaseapp.com",
+    projectId: "kwitter-hw-3dc3e",
+    storageBucket: "kwitter-hw-3dc3e.appspot.com",
+    messagingSenderId: "410092188637",
+    appId: "1:410092188637:web:d7d33d19fd7f32a6abfc34"
+  };
+
+  firebase.initializeApp(firebaseConfig);
